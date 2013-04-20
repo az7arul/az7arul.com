d9aace02c1890a8079a7e1e532934b2467282b14
Azhar <azhar@azhar.(none)>
2013-04-20 22:31:34 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+var
+  express = require( 'express' ),
+  app = express(),
+  path = require('path'),
+  poet = require( 'poet' )( app );
+
+// All default options, but shown for example
+
+poet
+  .createPostRoute()
+  .createPageRoute()
+  .createTagRoute()
+  .createCategoryRoute()
+  .init();
+
+app.set( 'view engine', 'jade' );
+app.set( 'views', __dirname + '/views' );
+app.use( express.static( __dirname + '/public' ));
+app.use( app.router );
+
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+  app.use(app.router);
+  app.use(require('less-middleware')({ src: __dirname + '/public' }));
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.get( '/', function ( req, res ) { res.render( 'index' ) });
+
+app.listen( 3000 );
